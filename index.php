aa59361285c11684be6bfd9c5730262ce20f056d
Jason Hansel <perljason@gmail.com>
2015-06-12 13:04:13 -0400

    starting to move away from server side rendering

--- index.php
+++ index.php
@@ -1,24 +1,79 @@
 
 <?php
-/**
- * Parsing callback for yaml tag.
- * @param mixed $value Data from yaml file
- * @param string $tag Tag that triggered callback
- * @param int $flags Scalar entity style (see YAML_*_SCALAR_STYLE)
- * @return mixed Value that YAML parser should emit for the given value
- */
-function tag_callback ($value, $tag, $flags) {
-  var_dump(func_get_args()); // debugging
-  return "Hello {$value}";
+
+require('vendor/autoload.php');
+require ('template.php');
+
+use Sdl\Parser\SdlParser;
+
+class Checkbox {	
+	public $label;
+	public $name;
+	function __construct($args) {
+		$this->label = $args['label'];
+		$this->name = $args['name'];
+	}
+	function render() {
+		// TODO fix xss
+		return '<div class="field"> <div class="ui  checkbox"> <input name="' . $this->name. '" type="checkbox"> <label>' . $this->label . '</label> </div> </div>';
+	}
 }
 
-$yaml = <<<YAML
-greeting: !example/hello World
-YAML;
+class Textbox {
+	public $label;
+	public $name;
+	function __construct($args) {
+		$this->label = $args['label'];
+		$this->name = $args['name'];
+	}
+	function render() {
+		// TODO fix xss
+		return '<div class="field"> <div class="ui  input"> <input name="' . $this->name. '" type="text"> <label>' . $this->label . '</label> </div> </div>';
+	}
+}
 
-$result = yaml_parse($yaml, 0, $ndocs, array(
-    '!example/hello' => 'tag_callback',
-  ));
+class Form {
+	public $items;
+	function __construct($args) {
+		$this->items = $args;
+	}
+	function render() {
+		$text = '<form action="submit.php" method="POST" class="ui form">';
+		foreach($this->items as $k => $x) {
+			if($x['type'] == 'checkbox') {
+				$x = new Checkbox($x);
+			} else {
+				$x = new Textbox($x);
+			}
+			$text .= $x->render();
+		}
+		return $text . ' <input type="Submit" value="hey" class="submit button" /> </form>';
+	}
+}
 
-var_dump($result);
+class Page {
+	public $form;
+	public $json;
+	function __construct($form, $json) {
+		$this->form = $form;
+		$this->json = $json;
+	}
+	function render() {
+		return template($this->form, $this->json);
+	}
+}
+
+
+$result = yaml_parse_file('forms/test.yml', 0, $ndocs);
+
+
+$json = json_encode($result);
+
+$form = new Form($result['fields']);
+
+$page = new Page($form, $json);
+
+
+echo $page->render();
+
 ?>
