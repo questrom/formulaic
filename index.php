1b1822e026de0a066cd4ce6d4f97a66f47fb6d4e
Jason Hansel <perljason@gmail.com>
2015-07-02 09:07:48 -0400

    caching

--- index.php
+++ index.php
@@ -3,5 +3,21 @@
 require('vendor/autoload.php');
 require('parts.php');
 
-$page = Parser::parse_jade('forms/test.jade');
-echo '<!DOCTYPE html>' . $page->get(new HTMLParentlessContext());
+$cache_enabled = false;
+
+// $time = microtime(true);
+
+$hash = sha1_file('forms/test.jade');
+$cache = './cache/' . $hash;
+
+if($cache_enabled && file_exists($cache)) {
+	$html = file_get_contents($cache);
+} else {
+	$page = Parser::parse_jade('forms/test.jade');
+	$html = '<!DOCTYPE html>' . $page->get(new HTMLParentlessContext());
+	file_put_contents($cache, $html);
+}
+
+// $time = microtime(true) - $time;
+
+echo $html;
