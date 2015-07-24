788c36537727c975d207d9234cb37a26a4190608
Jason Hansel <perljason@gmail.com>
2015-07-24 12:51:50 -0400

    Comments

--- client.js
+++ client.js
@@ -1,12 +1,17 @@
 /* jshint undef: true, unused: vars */
 /* globals $, document, FormData, window, navigator */
 
+// Detect iOS
 var isios = navigator.userAgent.match(/iPad|iPhone|iPod/g);
 if(isios) {
+	// Enable iOS-specific CSS
 	document.documentElement.className += ' ios';
 }
 
 function addPrompt(name, prompt) {
+	// Display a validation error next to the relevant form field.
+	// Return the element and its vertical position.
+
 	var $elem = $(document.getElementsByName(name))
 		.add($('[data-validation-name="' + name + '"]'))
 		.closest('.field:not(.not-validation-root), .validation-root')
@@ -23,12 +28,14 @@ function addPrompt(name, prompt) {
 }
 
 function removePrompts() {
+	// Remove all validation errors
 	$('.red.prompt').remove();
 	$('.field.error, .validation-root.error').removeClass('error');
 }
 
 
 // Based on code from jquery.inputmask
+// This is an input mask for dates in "m/d/y hh:mm:xm" format.
 $.extend($.inputmask.defaults.aliases, {
     'proper-datetime': {
         placeholder: "mm/dd/yyyy hh:mm am",
@@ -57,13 +64,15 @@ $.extend($.inputmask.defaults.aliases, {
     }
 });
 
+// Better modal transitions
 $.fn.modal.settings.transition='scale';
 
-
+// Update the label next to a slider with the slider's value
 function handleRange() {
 	$(this).parent().find('span.range-value').text($(this).val());
 }
 
+// Handle show-if conditions with checkboxes
 function handleBox() {
 	var $this = $(this);
 	var name = $this.attr('name');
@@ -74,6 +83,7 @@ function handleBox() {
 	$('[data-show-if-name="' + name + '"][data-show-if-condition="is-not-checked"]').toggle(!$this.is(':checked'));
 }
 
+// Handle show-if conditions with radio buttons
 function handleRadio() {
 	var $this = $(this);
 	var name = $this.attr('data-radio-group-name'),
@@ -82,12 +92,15 @@ function handleRadio() {
 	$('[data-show-if-name="' + name + '"][data-show-if-condition="is-radio-selected:' + value + '"]').show();
 }
 
+// Fix a form control's name before putting it in a list
 function fixName(groupName, currentName, num) {
 	return groupName + '[' + num + ']' + currentName.replace(/^(.*?)($|(?=\[))/,'[$1]');
 }
 
+// Enable all of the form controls within a particular element
 function enableFormControls(root) {
 
+	// "Add item" button for lists
 	root.find('.add-item').click(function() {
 		var $this = $(this);
 		var template = $this.closest('.list-items').find('> script').text();
@@ -122,10 +135,11 @@ function enableFormControls(root) {
 			$(this).closest('.segment').remove();
 		});
 
-
+		// Enable the newly-created form controls within the list
 		enableFormControls(item);
 	});
 
+	// Misc other controls
 	root.find('input[type=range]').on('input', handleRange).each(handleRange);
 	root.find('.ui.dropdown').dropdown();
 	root.find('input[type=checkbox]').on('change', handleBox).each(handleBox);
@@ -136,9 +150,11 @@ function enableFormControls(root) {
 
 $(function() {
 
+	// Get form controls working
 	enableFormControls($('form'));
 
 
+	// In case of server-side error
 	function doFail() {
 			$('.validation-error-message').hide();
 			var submit = $('[data-submit=true]').removeClass('loading').removeAttr('disabled');
@@ -146,15 +162,15 @@ $(function() {
 			$('.failure-modal').modal('show');
 	}
 
-	// alert('Hello world!');
 
 	if(isios) {
 		$('[data-submit=true]').on('touchend', function(e) {
-			// stackoverflow.com/questions/24306818/form-submit-button-event-is-not-captured-when-keyboard-is-up-ios-7
+			// see stackoverflow.com/questions/24306818/
 			$('form').submit();
 		});
 	}
 
+	// Handle form submission
 	$('form').on('submit', function(e) {
 
 
@@ -165,15 +181,19 @@ $(function() {
 			window.grecaptcha.reset();
 		}
 
+		// Remove validation prompts and show a Loading message
 		removePrompts();
 		$('[data-submit=true]').addClass('loading').attr('disabled', true);
 
-		var formData = new FormData($('form')[0]);
 
+		// This requires a relatively recent browser
+		// See http://stackoverflow.com/questions/10899384/
+
+		var formData = new FormData($('form')[0]);
 		$.ajax($('form').attr('action'), {
 			data: formData,
 			method: 'POST',
-			// http://stackoverflow.com/questions/10899384/uploading-both-data-and-files-in-one-form-using-ajax
+
 			contentType: false,
 			cache: false,
 			processData: false
@@ -182,14 +202,14 @@ $(function() {
 				x = JSON.parse(x);
 			} catch(e) {
 				$('.ui.form').append($('<p>').html(x));
-				console.log(x);
+				// console.log(x);
 				doFail();
 				return;
 			}
 			x = x.data;
 
 			if(x.errors) {
-				console.log(x.errors);
+				// console.log(x.errors);
 				$('.validation-error-message').show();
 				var submit = $('[data-submit=true]').removeClass('loading').removeAttr('disabled');
 				$(submit).find('span').text('Try Again');
@@ -205,18 +225,21 @@ $(function() {
 					}
 				}
 				if(lowest) {
+					// Scroll to the highest element with an error
 
 					lowest.elem[0].scrollIntoView();
-					if($('.ui.top.fixed.menu').css('position') === 'fixed') { // It isn't fixed on iOS
+					if($('.ui.top.fixed.menu').css('position') === 'fixed') {
+						// Unless we're on iOS, compensate for the top menu's height
 						window.scrollBy(0, -$('.ui.top.fixed.menu').height());
 					}
 				}
 
 			} else {
+
 				$('.validation-error-message').hide();
 				$('[data-submit=true]').removeClass('loading').removeAttr('disabled').find('span').text('Submit Again');
 
-
+				// Show debug output if in debug mode
 				var output = x.debugOutput;
 				if(output) {
 					$('.ui.form').append($('<p>').html(output));
